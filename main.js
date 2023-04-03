23defa3b9ba1712d9391fdfd42e7fc5a1da89c3e
Flavia Franco <104872642+flaviafraanco@users.noreply.github.com>
2023-04-03 17:55:22 -0300

    codigo funcionando

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-document.querySelector('.tecla_pom');
+// Trabalhando com todos elementos ao mesmo tempo
+function tocaSom(idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
+}
+    
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0
+
+//enquanto
+while (contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrumento}`;
+
+    //console.log(idAudio);
+
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+    //console.log(contador);
+}
